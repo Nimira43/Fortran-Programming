73459ebe31d40c108a6ebca47a5316ff171237d0
Nimira43 <n.rabone72@aol.com>
2025-12-14 04:32:43 +0000

    conditionals  - 2

--- introduction/conditionals.f90
+++ introduction/conditionals.f90
@@ -1,4 +1,12 @@
 program conditionals
   integer :: power_level
-  power_level = 450
+  power_level = 9450
 
+  if (power_level > 9000) then
+    print *, "Wow! Power over 9000!"
+  else if (power_level > 100 .and. power_level < 9000) then
+    print *, "A worthy warrior."
+  else
+    print *, "A mere imp in training."
+  end if
+end program conditionals
